3a0994c954c1eb510e126396815fbacd54df66f3
Peter Jones <pjones@redhat.com>
2020-05-29 10:59:33 -0400

    efisecdb: add efisecdb
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ src/efisecdb.c
@@ -0,0 +1,510 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * efisecdb.c - efi signature list management tool
+ * Copyright Peter Jones <pjones@redhat.com>
+ * Copyright Red Hat, Inc.
+ */
+#include "fix_coverity.h"
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include "efisec.h"
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+struct hash_param {
+	char *name;
+	efi_secdb_type_t algorithm;
+	ssize_t size;
+	bool def;
+};
+
+static struct hash_param hash_params[] = {
+	{.name = "sha512",
+	 .algorithm = SHA512,
+	 .size = 64,
+	 .def = false,
+	},
+	{.name = "sha256",
+	 .algorithm = SHA256,
+	 .size = 32,
+	 .def = true,
+	},
+	{.name = "sha1",
+	 .algorithm = SHA1,
+	 .size = 20,
+	 .def = false,
+	},
+};
+static int n_hash_params = sizeof (hash_params) / sizeof (hash_params[0]);
+
+static void
+set_hash_parameters(char *name, int *hash_number)
+{
+	FILE *out;
+	int def = -1;
+
+	if (strcmp(name, "help")) {
+		out = stderr;
+		for (int i = 0; i < n_hash_params; i++) {
+			if (!strcmp(name, hash_params[i].name)) {
+				*hash_number = i;
+				return;
+			}
+		}
+	} else {
+		out = stdout;
+	}
+
+	if (out == stderr)
+		warnx("Invalid hash type \"%s\"", name);
+	fprintf(out, "Supported hashes:");
+	for (int i = 0; i < n_hash_params; i++) {
+		fprintf(out, " %s", hash_params[i].name);
+		if (hash_params[i].def)
+			def = i;
+	}
+	fprintf(out, "\n");
+	if (def >= 0)
+		fprintf(out, "Default hash is %s\n", hash_params[def].name);
+	exit(out == stderr ? 1 : 0);
+}
+
+static int verbose_errors = 0;
+
+static void
+show_errors(void)
+{
+	int rc = 1;
+
+	if (!verbose_errors)
+		return;
+
+	printf("Error trace:\n");
+	for (int i = 0; rc > 0; i++) {
+		char *filename = NULL;
+		char *function = NULL;
+		int line = 0;
+		char *message = NULL;
+		int error = 0;
+
+		rc = efi_error_get(i, &filename, &function, &line, &message,
+				   &error);
+		if (rc < 0)
+			err(1, "error fetching trace value");
+		if (rc == 0)
+			break;
+		printf(" %s:%d %s(): %s: %s", filename, line, function,
+		       strerror(error), message);
+	}
+}
+
+static void NORETURN
+secdb_err(int status, const char * const fmt, ...)
+{
+	va_list ap;
+	int errnum = errno;
+
+	fprintf(stderr, "%s: ", program_invocation_short_name);
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	errno = errnum;
+	fprintf(stderr, ": %m\n");
+	show_errors();
+	exit(status);
+}
+
+static void NORETURN
+secdb_errx(int status, const char * const fmt, ...)
+{
+	va_list ap;
+	int errnum = errno;
+
+	fprintf(stderr, "%s: ", program_invocation_short_name);
+	va_start(ap, fmt);
+	errno = errnum;
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	show_errors();
+	exit(status);
+}
+
+static void NORETURN
+usage(int status)
+{
+	fprintf(status == 0 ? stdout : stderr,
+		"Usage: %s [OPTION...]\n"
+		"  -i, --infile=<file>       input database\n"
+		"  -o, --outfile=<file>      output database\n"
+		"  -a, --add                 following hashes or certs are to be added (default)\n"
+		"  -r, --remove              following hashes or certs are to be removed\n"
+		"  -g, --owner-guid=<GUID>   following added entries use GUID as the owner\n"
+		"  -h, --hash=<hash>         hash value to add (\n"
+		"  -t, --type=<hash-type>    hash type to add (\"help\" lists options)\n"
+		"  -c, --certificate=<file>  certificate file to add\n",
+		program_invocation_short_name);
+	exit(status);
+}
+
+typedef enum {
+	ADD,
+	REMOVE
+} action_type_t;
+
+typedef struct {
+	list_t list;
+
+	action_type_t action;
+	efi_guid_t owner;
+	efi_secdb_type_t algorithm;
+	uint8_t *data;
+	size_t datasz;
+} action_t;
+#define for_each_action(pos, head) list_for_each(pos, head)
+#define for_each_action_safe(pos, n, head) list_for_each_safe(pos, n, head)
+
+static void
+action_add(list_t *list, action_type_t action_type,
+	   const efi_guid_t *owner,
+	   efi_secdb_type_t algorithm, uint8_t *data, size_t datasz)
+{
+	action_t *action;
+
+	if (action_type == ADD && efi_guid_is_empty(owner))
+		errx(1, "no owner spefified for --add");
+
+	action = calloc(1, sizeof(action_t));
+	if (!action)
+		err(1, "could not allocate memory");
+	action->action = action_type;
+	action->owner = *owner;
+	action->algorithm = algorithm;
+	action->data = data;
+	action->datasz = datasz;
+	list_add_tail(&action->list, list);
+}
+
+static void
+free_actions(int status UNUSED, void *actionsp)
+{
+	list_t *actions = (list_t *)actionsp;
+	list_t *pos, *tmp;
+
+	for_each_action_safe(pos, tmp, actions) {
+		action_t *action = list_entry(pos, action_t, list);
+
+		list_del(&action->list);
+		xfree(action->data);
+		free(action);
+	}
+}
+
+static void
+free_infiles(int status UNUSED, void *infilesp)
+{
+	list_t *infiles = (list_t *)infilesp;
+	list_t *pos, *tmp;
+
+	for_each_ptr_safe(pos, tmp, infiles) {
+		ptrlist_t *entry = list_entry(pos, ptrlist_t, list);
+
+		xfree(entry->ptr);
+		list_del(&entry->list);
+		free(entry);
+	}
+}
+
+static void
+maybe_free_secdb(int status UNUSED, void *voidp)
+{
+	efi_secdb_t **secdbp = (efi_secdb_t **)voidp;
+
+	if (secdbp == NULL || *secdbp == NULL)
+		return;
+
+	efi_secdb_free(*secdbp);
+}
+
+static void
+maybe_do_unlink(int status, void *filep)
+{
+	char **file = (char **)filep;
+
+	if (status == 0)
+		return;
+	if (file == NULL || *file == NULL)
+		return;
+
+	unlink(*file);
+}
+
+static void
+check_hash_index(int hash_index)
+{
+	if (hash_index < 0 || hash_index >= n_hash_params)
+		errx(1, "hash type is not set");
+}
+
+static efi_secdb_t *secdb;
+static list_t infiles;
+static list_t actions;
+static char *outfile = NULL;
+
+int
+main(int argc, char *argv[])
+{
+	efi_guid_t owner = efi_guid_empty;
+	int rc;
+	action_type_t mode = ADD;
+	list_t *pos, *tmp;
+	int c, i;
+	int hash_index = -1;
+	bool force = false;
+	int verbose = 0;
+	bool dump = false;
+	bool annotate = false;
+	bool wants_add_actions = false;
+
+	const char sopts[] = ":aAc:dfg:h:i:o:rt:v?";
+	const struct option lopts[] = {
+		{"add", no_argument, NULL, 'a' },
+		{"annotate", no_argument, NULL, 'A' },
+		{"certificate", required_argument, NULL, 'c' },
+		{"dump", no_argument, NULL, 'd' },
+		{"force", no_argument, NULL, 'f' },
+		{"owner-guid", required_argument, NULL, 'g' },
+		{"hash", required_argument, NULL, 'h' },
+		{"infile", required_argument, NULL, 'i' },
+		{"outfile", required_argument, NULL, 'o' },
+		{"remove", no_argument, NULL, 'r' },
+		{"type", required_argument, NULL, 't' },
+		{"verbose", no_argument, NULL, 'v' },
+		{"usage", no_argument, NULL, '?' },
+		{"help", no_argument, NULL, '?' },
+		{NULL, 0, NULL, '\0' }
+	};
+
+	INIT_LIST_HEAD(&infiles);
+	INIT_LIST_HEAD(&actions);
+
+	on_exit(free_actions, &actions);
+	on_exit(free_infiles, &infiles);
+	on_exit(maybe_free_secdb, &secdb);
+	on_exit(maybe_do_unlink, &outfile);
+
+	/*
+	 * parse the command line.
+	 *
+	 * note that we don't really process the security database inputs,
+	 * here, and the cert and hash add/remove must be kept in order as
+	 * supplied.
+	 */
+	opterr = 0;
+	while ((c = getopt_long(argc, argv, sopts, lopts, &i)) != -1) {
+		uint8_t *data;
+		ssize_t datasz;
+
+		switch (c) {
+		case 'a':
+			mode = ADD;
+			break;
+		case 'A':
+			dump = true;
+			annotate = true;
+			break;
+		case 'c':
+			datasz = get_file(&data, "%s", optarg);
+			if (datasz < 0)
+				secdb_err(1, "could not read certificate \"%s\"",
+					  optarg);
+			datasz -= 1;
+
+			// this is arbitrary but still much too small
+			if (datasz < 16)
+				secdb_err(1, "certificate \"%s\" is invalid",
+					  optarg);
+
+			debug("%s certificate of %d bytes", mode == ADD ? "adding" : "removing", datasz);
+			if (mode == ADD)
+				wants_add_actions = true;
+			action_add(&actions, mode, &owner, X509_CERT, data, datasz);
+			break;
+		case 'd':
+			dump = true;
+			break;
+		case 'f':
+			force = true;
+			break;
+		case 'g':
+			rc = efi_id_guid_to_guid(optarg, &owner);
+			if (rc < 0)
+				secdb_errx(1, "could not parse guid \"%s\"", optarg);
+			break;
+		case 'h':
+			check_hash_index(hash_index);
+			datasz = strlen(optarg);
+			if (datasz != hash_params[hash_index].size * 2)
+				secdb_errx(1,
+					   "hash \"%s\" requires a %zd-bit value, but supplied value is %zd bits",
+					   hash_params[hash_index].name,
+					   hash_params[hash_index].size * 8,
+					   datasz * 4);
+			datasz >>= 1;
+			data = hex_to_bin(optarg, datasz);
+			debug("%s hash %s", mode == ADD ? "adding" : "removing", optarg);
+			if (mode == ADD)
+				wants_add_actions = true;
+			action_add(&actions, mode, &owner,
+				   hash_params[hash_index].algorithm,
+				   data, datasz);
+			break;
+		case 'i':
+			ptrlist_add(&infiles, optarg);
+			break;
+		case 'o':
+			if (outfile)
+				secdb_errx(1, "--outfile cannot be used multiple times.");
+			outfile = optarg;
+			break;
+		case 'r':
+			mode = REMOVE;
+			break;
+		case 't':
+			set_hash_parameters(optarg, &hash_index);
+			break;
+		case 'v':
+			if (optarg) {
+				long v;
+
+				errno = 0;
+				v = strtol(optarg, NULL, 0);
+				verbose = (errno == ERANGE) ? verbose + 1 : v;
+			} else {
+				verbose += 1;
+			}
+			break;
+		case '?':
+			usage(0);
+			break;
+		case ':':
+			errx(1, "option '%c' does not take an argument (\"%s\")", optopt, optarg);
+		}
+	}
+
+	setenv("NSS_DEFAULT_DB_TYPE", "sql", 0);
+	efi_set_verbose(verbose, stderr);
+
+	if (!outfile && !dump)
+		errx(1, "no output specified");
+	if (list_empty(&infiles) && !wants_add_actions)
+		errx(1, "no input files or database additions");
+
+	secdb = efi_secdb_new();
+	if (!secdb)
+		err(1, "could not allocate memory");
+	debug("top secdb:%p", secdb);
+
+	for_each_ptr_safe(pos, tmp, &infiles) {
+		int infd = -1;
+		uint8_t *siglist = NULL;
+		size_t siglistsz = 0;
+		char *infile;
+		ptrlist_t *entry = list_entry(pos, ptrlist_t, list);
+
+		infile = entry->ptr;
+
+		debug("adding input file %s", infile);
+		infd = open(infile, O_RDONLY);
+		if (infd < 0)
+			err(1, "could not open \"%s\"", infile);
+
+		rc = read_file(infd, &siglist, &siglistsz);
+		if (rc < 0)
+			err(1, "could not read \"%s\"", infile);
+		siglistsz -= 1;
+		close(infd);
+
+		rc = efi_secdb_parse(siglist, siglistsz, &secdb);
+		if (rc < 0) {
+			/* haaaack city */
+			if (siglistsz > 4
+			    && !(*(uint32_t *)siglist & ~0x7ffu))
+				rc = efi_secdb_parse(&siglist[4], siglistsz-4,
+						     &secdb);
+			if (rc < 0)
+				secdb_err(1, "could not parse input file \"%s\"", infile);
+		}
+		xfree(siglist);
+		list_del(&entry->list);
+		free(entry);
+	}
+
+	if (!outfile && !dump)
+		errx(1, "no output file specified");
+
+	for_each_action_safe(pos, tmp, &actions) {
+		action_t *action = list_entry(pos, action_t, list);
+
+		if (action->action == ADD) {
+			debug("adding %d entry", action->algorithm);
+			efi_secdb_add_entry(secdb, &action->owner,
+					    action->algorithm,
+					    (efi_secdb_data_t *)action->data,
+					    action->datasz);
+		} else {
+			debug("removing %d entry", action->algorithm);
+			efi_secdb_del_entry(secdb, &action->owner,
+					    action->algorithm,
+					    (efi_secdb_data_t *)action->data,
+					    action->datasz);
+		}
+		list_del(&action->list);
+		free(action->data);
+		free(action);
+	}
+
+	if (dump)
+		secdb_dump(secdb, annotate);
+
+	if (!outfile)
+		exit(0);
+
+	int outfd = -1;
+	int flags = O_WRONLY|O_CREAT | (force ? 0 : O_EXCL);
+
+	debug("adding output file %s", outfile);
+	outfd = open(outfile, flags, 0600);
+	if (outfd < 0)
+		err(1, "could not open \"%s\"", outfile);
+
+	ftruncate(outfd, 0);
+
+	void *output;
+	size_t size = 0;
+	rc = efi_secdb_realize(secdb, &output, &size);
+	if (rc < 0)
+		secdb_err(1, "could not realize signature list");
+
+	rc = write(outfd, output, size);
+	if (rc < 0)
+		err(1, "could not write signature list");
+
+	ftruncate(outfd, size);
+	close(outfd);
+	xfree(output);
+
+	return 0;
+}
---
+++ src/secdb-dump.c
@@ -0,0 +1,238 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * secdb-dump.c - efi_secdb_t hex-dump + annotation
+ * Copyright Peter Jones <pjones@redhat.com>
+ */
+
+#include "efisec.h"
+#include "hexdump.h"
+
+static bool annotate = false;
+static char *buf = NULL;
+static size_t bufsz = 0;
+static size_t start = 0;
+
+static inline void
+secdb_dump_finish(void)
+{
+	if (buf && bufsz) {
+		hexdumpat((uint8_t *)buf, bufsz, start);
+		free(buf);
+	}
+	buf = NULL;
+	bufsz = 0;
+}
+
+static inline ssize_t
+secdb_buffer(char *val, size_t valsz, size_t offset)
+{
+	char *nbuf;
+	size_t nbufsz, allocsz;
+
+	if (!buf) {
+		// just because valgrind is annoying about realloc(NULL, ...)
+		buf = calloc(1, page_size);
+		if (!buf)
+			return -1;
+	}
+
+	nbufsz = offset + valsz;
+	allocsz = ALIGN_UP(nbufsz, page_size);
+	nbuf = realloc(buf, allocsz);
+	if (!nbuf) {
+		free(buf);
+		buf = NULL;
+		bufsz = 0;
+		return 0;
+	}
+	memset(nbuf + bufsz, '\0', allocsz - bufsz);
+	memcpy(nbuf + offset, val, valsz);
+	buf = nbuf;
+	bufsz = nbufsz;
+	return nbufsz;
+}
+
+static inline size_t
+secdb_dump_value(char *val, size_t size, size_t offset, char *fmt, ...)
+{
+	char posbuf[9];
+	char hexbuf[49];
+	char textbuf[19];
+	posbuf[sizeof(hexbuf)-1] = '\0';
+	hexbuf[sizeof(hexbuf)-1] = '\0';
+	textbuf[sizeof(textbuf)-1] = '\0';
+	size_t printed = 0;
+	va_list ap;
+	bool once = false;
+
+	if (!annotate) {
+		if (size == 0)
+			return offset;
+		return secdb_buffer(val, size, offset);
+	}
+	if (size == 0 && strlen(fmt) == 0)
+		return offset;
+
+	if (size == 0) {
+		memset(posbuf, ' ', sizeof(posbuf)-1);
+		memset(hexbuf, ' ', sizeof(hexbuf)-1);
+		memset(textbuf, ' ', sizeof(textbuf)-1);
+
+		sprintf(posbuf, "%08zx", offset + printed);
+		prepare_text("", 0, textbuf, offset + printed);
+		//memcpy(textbuf, "||", 2);
+		unsigned int i = 0;
+		for (i = strlen(textbuf); annotate && i < sizeof(textbuf)-1; i++)
+			textbuf[i] = ' ';
+		textbuf[sizeof(textbuf)-1] = '\0';
+		printf("%s  %s  %s", posbuf, hexbuf, textbuf);
+		if (annotate) {
+			printf("  ");
+			va_start(ap, fmt);
+			vprintf(fmt, ap);
+			va_end(ap);
+		}
+		printf("\n");
+	}
+	while (size - printed) {
+		size_t sz;
+
+		memset(posbuf, ' ', sizeof(posbuf)-1);
+		memset(hexbuf, ' ', sizeof(hexbuf)-1);
+		memset(textbuf, ' ', sizeof(textbuf)-1);
+
+		sprintf(posbuf, "%08zx", offset + printed);
+		debug("size:%zd printed:%zd", size, printed);
+		sz = prepare_hex(val+printed, size-printed, hexbuf, offset+printed);
+		prepare_text(val+printed, size-printed, textbuf, offset+printed);
+		unsigned int i = 0;
+		for (i = strlen(textbuf); annotate && i < sizeof(textbuf)-1; i++)
+			textbuf[i] = ' ';
+		textbuf[sizeof(textbuf)-1] = '\0';
+
+		//printf("printed %zd -> %zd\n", printed, printed+sz);
+		printed += sz;
+
+		printf("%s  %s  %s", posbuf, hexbuf, textbuf);
+		if (!once && annotate) {
+			printf("  ");
+			va_start(ap, fmt);
+			vprintf(fmt, ap);
+			va_end(ap);
+			once = true;
+		}
+		printf("\n");
+		//printf("size %zd - printed %zd = %zd\n", size, printed, size-printed);
+	}
+
+	return offset + printed;
+}
+
+static inline size_t
+secdb_dump_esl(efi_secdb_t *secdb, int esl, size_t offset)
+{
+	const efi_guid_t *alg;
+	char *id_guid = NULL;
+
+	alg = secdb_guid_from_type(secdb->algorithm);
+	if (!alg)
+		alg = &efi_guid_empty;
+	efi_guid_to_id_guid(alg, &id_guid);
+	offset = secdb_dump_value((char *)alg, sizeof(efi_guid_t), offset,
+				  "esl[%d].signature_type = %s", esl, id_guid);
+	xfree(id_guid);
+
+	offset = secdb_dump_value((char *)&secdb->listsz,
+				  sizeof(secdb->listsz), offset,
+				  "esl[%d].signature_list_size = %d",
+				  esl, secdb->listsz);
+
+	offset = secdb_dump_value((char *)&secdb->hdrsz,
+				  sizeof(secdb->hdrsz), offset,
+				  "esl[%d].signature_header_size = %d",
+				  esl, secdb->hdrsz);
+
+	offset = secdb_dump_value((char *)&secdb->sigsz,
+				  sizeof(secdb->sigsz), offset,
+				  "esl[%d].signature_size = %d",
+				  esl, secdb->sigsz);
+
+	offset = secdb_dump_value((char *)NULL, secdb->hdrsz, offset,
+				  "esl[%d].signature_header (end:0x%08x)",
+				  esl, offset + secdb->hdrsz);
+
+	return offset;
+}
+
+static inline size_t
+secdb_dump_esd(secdb_entry_t *entry, int esl, int esd,
+	       size_t data_size, size_t offset)
+{
+	char *id_guid = NULL;
+
+	efi_guid_to_id_guid(&entry->owner, &id_guid);
+	offset = secdb_dump_value((char *)&entry->owner,
+				  sizeof(efi_guid_t), offset,
+				  "esl[%d].signature[%d].owner = %s",
+				  esl, esd, id_guid);
+	xfree(id_guid);
+	offset = secdb_dump_value((char *)&entry->data, data_size, offset,
+				  "esl[%d].signature[%d].data (end:0x%08zx)",
+				  esl, esd, offset+data_size);
+	return offset;
+}
+
+void
+secdb_dump(efi_secdb_t *secdb, bool annotations)
+{
+	int esln = 0;
+	list_t *pos0, *pos1;
+	ssize_t offset = 0;
+
+	start = offset;
+	annotate = annotations;
+
+	for_each_secdb(pos0, &secdb->list) {
+		efi_secdb_t *esl;
+		int esdn = 0;
+
+		esl = list_entry(pos0, efi_secdb_t, list);
+
+		debug("esl[%d]:%p", esln, esl);
+		offset = secdb_dump_esl(esl, esln, offset);
+
+		for_each_secdb_entry(pos1, &esl->entries) {
+			secdb_entry_t *esd;
+			bool has_owner = true;
+			size_t datasz;
+			int rc;
+
+			rc = secdb_entry_has_owner_from_type(esl->algorithm,
+							     &has_owner);
+			if (rc < 0) {
+				int saved_errno = errno;
+				secdb_dump_finish();
+				errno = saved_errno;
+				efi_error("could not determine signature type");
+				return;
+			}
+			datasz = esl->sigsz
+				 - (has_owner ? sizeof(efi_guid_t) : 0);
+
+			esd = list_entry(pos1, secdb_entry_t, list);
+			debug("esl[%d].esd[%d]:%p owner:%p data:%p-%p datasz:%zd",
+			      esln, esdn, esd, &esd->owner,
+			      &esd->data, &esd->data+datasz, datasz);
+			offset = secdb_dump_esd(esd, esln, esdn, datasz, offset);
+			esdn += 1;
+		}
+
+		esln += 1;
+	}
+	secdb_dump_finish();
+	printf("%08lx\n", offset);
+
+	fflush(stdout);
+}
+
+// vim:fenc=utf-8:tw=75:noet
